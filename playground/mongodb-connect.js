228127eb9d3923b75cdacc79837055ff628c14e0
Nikhil Garg <nikhilgarg72@gmail.com>
2017-06-29 15:06:05 +0530

    Added find methods

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,4 +1,4 @@
-const MongoClient = require('mongodb').MongoClient;
+const {MongoClient} = require('mongodb');
 
 MongoClient.connect(`mongodb://localhost:27017/TodoApp`, (err, db) => {
     if (err) {
