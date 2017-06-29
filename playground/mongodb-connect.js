d41b8935bedd4bab79b63dfabe0ba8c248880bf1
Nikhil Garg <nikhilgarg72@gmail.com>
2017-06-29 14:11:34 +0530

    installed mongodb native driver, connected to mongodb, created new collections and inserted new docs into them

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(`mongodb://localhost:27017/TodoApp`, (err, db) => {
+    if (err) {
+        return console.log("Unable to connect to mongodb server", err);
+    }
+    console.log("Connected to mongodb server");
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    },(err,res)=>{
+        if(err){
+            return console.log("Unable to insert todo",err);
+        }
+
+        console.log(JSON.stringify(res.ops,undefined, 2));
+    });
+
+    db.collection('Users').insertOne({
+        name: "Nikhil",
+        age: 24,
+        location: "Bangalore"
+    }, (err, res) => {
+        if (err) {
+            return console.log("Unable to insert todo", err);
+        }
+
+        console.log(JSON.stringify(res.ops, undefined, 2));
+    });
+
+
+    db.close();
+}); //amazon webservices or heroku url or localhost url
