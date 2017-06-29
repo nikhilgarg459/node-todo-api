1d309e015119d573f331956dd30e19e2968163a8
Nikhil Garg <nikhilgarg72@gmail.com>
2017-06-30 01:25:46 +0530

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,30 +1,42 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 var app = express();
 
 app.use(bodyParser.json());
-app.post('/todos', (req,res)=>{
+app.post('/todos', (req, res) => {
     // console.log(req.body);
     var todo = new Todo({
         text: req.body.text
     });
 
-    todo.save().then((doc)=>res.status(200).send(doc)).catch(err=>res.status(400).send(err));
+    todo.save().then((doc) => {
+        res.status(200).send(doc)
+    }).catch((err) => {
+        res.status(400).send(err)
+    });
 
 });
 
-app.listen(port,()=>{
+app.get('/todos', (req, res) => {
+    Todo.find({}).then((todos) => {
+        res.status(200).send({ todos });
+    }).catch((err) => {
+        res.status(400).send(err);
+    })
+});
+
+app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
 
-module.exports = {app};
+module.exports = { app };
 
 
 
