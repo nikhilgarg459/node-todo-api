017e6db67a284229f85f1c52bf9d83847ce336e3
Nikhil Garg <nikhilgarg72@gmail.com>
2017-07-01 22:36:09 +0530

    Added tests for  GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.get('/todos/:id',(req, res)=>{
            return res.status(404).send();
         }
 
-        res.send({todo});
+        res.status(200).send({todo});
 
     }).catch((err)=>{
        return res.status(400).send();
